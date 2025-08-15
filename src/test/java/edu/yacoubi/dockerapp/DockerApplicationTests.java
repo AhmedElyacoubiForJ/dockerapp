aaa1eac2bc52658365e6757b64f1a76ad9cb98e8
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2025-08-15 12:02:54 +0200

    initial commit

---
+++ src/test/java/edu/yacoubi/dockerapp/DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yacoubi.dockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
